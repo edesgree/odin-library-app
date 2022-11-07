4950f1a1135af12b6b0d0bf164f2ecf8680cbc6e
Edouard Desgrée <edouard.desgree@gmail.com>
2022-11-07 16:05:21 +0100

    book card design

--- public/script.js
+++ public/script.js
@@ -1,7 +1,13 @@
 // app
 const app = () => {
+    const iconsBook = document.querySelectorAll(".icon-book .icon-book-cover");
+    
+    //color random for each new book icon
+    iconsBook.forEach(icon => icon.setAttribute("style","fill-rule:evenodd;clip-rule:evenodd;fill:rgb("+rand(255)+", "+rand(255)+", "+rand(255)+")"));
 
 }
-
+function rand(max) {
+    return Math.floor(Math.random() * (max + 1));
+};
 //play app
 app();
