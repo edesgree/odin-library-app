cb2269fa47af0a0af34e52b55a708d09e275b9a2
Edouard Desgrée <edouard.desgree@gmail.com>
2022-11-09 08:36:37 +0100

    modal button close

--- public/script.js
+++ public/script.js
@@ -216,7 +216,7 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   // Add a click event on various child elements to close the parent modal
-  (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete') || []).forEach(($close) => {
+  (document.querySelectorAll('[data-close-modal],.modal-background, .modal-close, .modal-card-head .delete') || []).forEach(($close) => {
     const $target = $close.closest('.modal');
 
     $close.addEventListener('click', () => {
