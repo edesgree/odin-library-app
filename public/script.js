312056987cb13beee9a7932de3b8c3e34c2f8487
Edouard Desgrée <edouard.desgree@gmail.com>
2022-12-10 23:54:05 +0100

    modal closing with form

--- public/script.js
+++ public/script.js
@@ -200,7 +200,7 @@ const app = () => {
     // if form is valid, we call add book
     if (checkForm()) {
       addBook();
-      modalAddBook.classList.remove('is-active');
+      closeModal(modalAddBook);
       addBookForm.reset();
     }
   });
@@ -222,22 +222,21 @@ const app = () => {
 };
 
 //modal support
+// Functions to open and close a modal
+function openModal($el) {
+  $el.classList.add('is-active');
+}
+
+function closeModal($el) {
+  $el.classList.remove('is-active');
+}
+
+function closeAllModals() {
+  (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+    closeModal($modal);
+  });
+}
 document.addEventListener('DOMContentLoaded', () => {
-  // Functions to open and close a modal
-  function openModal($el) {
-    $el.classList.add('is-active');
-  }
-
-  function closeModal($el) {
-    $el.classList.remove('is-active');
-  }
-
-  function closeAllModals() {
-    (document.querySelectorAll('.modal') || []).forEach(($modal) => {
-      closeModal($modal);
-    });
-  }
-
   // Add a click event on buttons to open a specific modal
   (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
     const modal = $trigger.dataset.target;
